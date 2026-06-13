523b4026231d9b535a7bf8d73b8e2633d45d7c1f
ben-nightworks <ben@nightworks.dev>
2026-06-13 21:01:59 +1000

    Fixed includes

--- days-since-last-tracker/src/App.tsx
+++ days-since-last-tracker/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import DaysSinceIncident from './components/days-since-last'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
